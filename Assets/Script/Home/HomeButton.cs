74c72cea3d2a3c835f6d9b95f37c053e0e77b2eb
Sandou <yatagarasu3421@outlook.jp>
2020-01-11 16:20:51 +0900

    Edit Home

---
+++ Assets/Script/Home/HomeButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ClikerGame;
+
+public class HomeButton : MonoBehaviour
+{
+    [SerializeField] float Weght;
+    [SerializeField] float Clere;
+
+    [SerializeField] Text _text = null;
+    [SerializeField] string name;
+    private void Start()
+    {
+        _text.text = name;
+    }
+    public void OnButton()
+    {
+        GameManeger.Clere = Clere;
+        GameManeger.WeghtOver = Weght;
+    }
+}
--- Assets/Script/Home/HomeManeger.cs
+++ Assets/Script/Home/HomeManeger.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class HomeManeger : MonoBehaviour
 {
+    GameObject  LastLayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
